3bb5e1178575287b0bb973f4da88373c1db817ef
Maro Huang <maro.huang@hukaka.com>
2020-10-19 23:13:25 +0800

    把帳密碼掉

--- git_clone.sh
+++ git_clone.sh
@@ -4,4 +4,4 @@
 #2. 移動到預定建立專案資料夾的上層目錄
 #3. 執行 sh git_clone.sh <GitHub庫名稱> <專案資料夾名稱>
 
-git clone https://ayuhamaro:2rh0weFmg7c@github.com/$1 $2
+git clone https://<your_github_account>:<your_github_password>@github.com/$1 $2
